ac4a8fe62eef3cc7e75986aa363e04aaf8d8b8ed
Noelia Barrionuevo <noe.br97@gmail.com>
2025-11-15 19:17:27 +0100

    feat: add funcionalidad para registrar un usuario en el modal y crearlo en bbdd

--- Hombres lobo/frontend/src/main.ts
+++ Hombres lobo/frontend/src/main.ts
@@ -1,30 +1,61 @@
 import './style.css'
 import api from './api'
+import { validarPass, registrarUsuario } from './validarFormularioRegistro'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <div class="app">
-    <h1>Frontend Vite conectado a Laravel</h1>
-    <button id="cargar-datos" type="button">
-      Probar endpoint /ping
-    </button>
-    <pre id="resultado" class="resultado"></pre>
-  </div>
-`
+// app.innerHTML = `
+//   <div class="app">
+//     <h1>Frontend Vite conectado a Laravel</h1>
+//     <button id="cargar-datos" type="button">
+//       Probar endpoint /ping
+//     </button>
+//     <pre id="resultado" class="resultado"></pre>
+//   </div>
+// `
 
-const boton = document.querySelector<HTMLButtonElement>('#cargar-datos')!
-const resultado = document.querySelector<HTMLPreElement>('#resultado')!
+// const boton = document.querySelector<HTMLButtonElement>('#cargar-datos')!
+// const resultado = document.querySelector<HTMLPreElement>('#resultado')!
 
-boton.addEventListener('click', async () => {
-  resultado.textContent = 'Cargando...'
+// boton.addEventListener('click', async () => {
+//   resultado.textContent = 'Cargando...'
 
-  try {
-    const respuesta = await api.get('/ping') // 👈 coincide con Route::get('/ping', ...)
+//   try {
+//     const respuesta = await api.get('/ping') // 👈 coincide con Route::get('/ping', ...)
 
-    resultado.textContent = JSON.stringify(respuesta.data, null, 2)
-  } catch (error) {
-    console.error(error)
-    resultado.textContent = 'Error llamando a la API (mira la consola)'
-  }
+//     resultado.textContent = JSON.stringify(respuesta.data, null, 2)
+//   } catch (error) {
+//     console.error(error)
+//     resultado.textContent = 'Error llamando a la API (mira la consola)'
+//   }
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+  const formulario = document.getElementById('formulario_registro')
+
+  formulario?.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const nombre = (document.getElementById('nombre_registro') as HTMLInputElement).value
+    const apellido1 = (document.getElementById('apellido_registro') as HTMLInputElement).value
+    const apellido2 = (document.getElementById('apellido2_registro') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const nick = (document.getElementById('username_registro') as HTMLInputElement).value
+    const password = (document.getElementById('password_registro') as HTMLInputElement).value
+
+    const respuesta = await registrarUsuario({
+      nombre,
+      apellido1,
+      apellido2,
+      email,
+      nick,
+      password
+    })
+
+    if (respuesta.ok) {
+      console.log('Usuario creado: ', respuesta.usuario)
+    } else {
+      console.log('Error: ', respuesta)
+    }
+    //validarPass()
+  })
 })
--- Hombres lobo/frontend/src/validarFormularioRegistro.ts
+++ Hombres lobo/frontend/src/validarFormularioRegistro.ts
@@ -1,8 +1,12 @@
+import api from './api'
+
 const formulario = document.getElementById('formulario_registro')
 
 export function validarPass () {
     const pass = (document.getElementById('password_registro') as HTMLInputElement).value
     const errorMsg = document.getElementById('password_error') as HTMLElement
+    const validMsg = document.getElementById('usuario_registrado') as HTMLElement
+    
     let passReg = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     let resultado = passReg.test(pass)
     if (!resultado) {
@@ -11,5 +15,20 @@ export function validarPass () {
     } else {
         errorMsg.textContent = ''
         errorMsg.classList.remove('visible')
+    }
+}
+
+export async function registrarUsuario(datos:any) {
+    try {
+        console.log(datos)
+        const res = await api.post('/usuarios/registrar', datos)
+        console.log(res)
+        return res.data
+    } catch (error:any) {
+        if(error.response) {
+            return error.response.data
+        }
+        console.error('Error insesperado: ', error)
+        return {ok: false};
     }
 }
