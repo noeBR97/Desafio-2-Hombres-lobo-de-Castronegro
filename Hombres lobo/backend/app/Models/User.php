ce8b5b52b7dc55be2828239ddf0eb1b8fd0b3d74
Noelia Barrionuevo <noe.br97@gmail.com>
2025-11-16 12:07:20 +0100

    refactor

--- Hombres lobo/backend/app/Models/User.php
+++ Hombres lobo/backend/app/Models/User.php
@@ -8,14 +8,8 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'nombre',
         'apellido1',
@@ -27,22 +21,11 @@ class User extends Authenticatable
         'partidas_ganadas',
         'partidas_perdidas',
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
 
     protected $casts = [
         'partidas_jugadas'   => 'integer',
@@ -50,12 +33,4 @@ class User extends Authenticatable
         'partidas_perdidas'  => 'integer',
         'email_verified_at'  => 'datetime',
     ];
-
-    // protected function casts(): array
-    // {
-    //     return [
-    //         'email_verified_at' => 'datetime',
-    //         'password' => 'hashed',
-    //     ];
-    // }
 }
