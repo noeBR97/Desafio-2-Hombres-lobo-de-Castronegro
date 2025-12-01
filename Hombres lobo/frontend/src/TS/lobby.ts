ff47c1d3365f27776c626561996cb41cca6ab2a3
Diego <diegogarciaangel25@gmail.com>
2025-12-01 13:52:56 +0100

    cambio en el diseño del juego

--- Hombres lobo/frontend/src/TS/lobby.ts
+++ Hombres lobo/frontend/src/TS/lobby.ts
@@ -177,7 +177,6 @@ function controlBotones() {
     if (btnIniciar) {
         btnIniciar.addEventListener('click', () => {
             console.log("¡Iniciando partida!");
-            // alert("Funcionalidad de iniciar partida no implementada.");
             const gameId = getGameIdFromUrl();
             window.location.href = `/HTML/juego.html?id=${gameId}`;
         });
