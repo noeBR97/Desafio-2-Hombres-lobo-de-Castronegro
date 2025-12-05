f2fb0965e8a7f0e5423adcb4eef49c70372cd062
Aguila <joseaguila3012@gmail.com>
2025-12-05 14:07:28 +0100

    aumento velocidad

--- Hombres lobo/frontend/src/api.ts
+++ Hombres lobo/frontend/src/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const baseURL = window.location.hostname === 'localhost'
-  ? 'http://localhost:8000' // navegador local
-  : 'http://backend:8000';  // front dentro de Docker
-
-const api = axios.create({ baseURL });
+const api = axios.create({
+  baseURL: `${window.location.origin}`,
+});
 
 export default api;
